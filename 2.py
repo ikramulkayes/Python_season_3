cb13045e5bf57f0354c21e3ef2b2f764e8059828
Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2022-06-15 21:44:48 +0600

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+lst = []
+while True:
+    k = input()
+    if k == "STOP":
+        break
+    else:
+        k = k.split()
+        lst.append(k)
+
+times = int(input())
+for elm in range(times):
+    k = input().split()
+    lst.append()
