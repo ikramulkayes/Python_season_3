267c18911b8c34dd4fdd199d878e4348a9cb5eb9
Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2022-08-31 02:43:22 +0600

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,18 +1,7 @@
-def phonebook(lst):
-  nlst =[]
-  dct = {}
-  for i in range(len(lst)//2):
-    nlst.append([lst[0+2*i], lst[1+2*i]])
-    if nlst[0][0][0] not in dct.keys():
-      dct[nlst[0][0][0]] = {nlst[0][0]: [nlst[-1]]}
-    else:
-      if nlst[0][0] not in dct[nlst[0][0[0]].keys():
-         dct[nlst[0][0][0]][nlst[0][0]] = [nlst[-1]]
-      else:
-         var = dct[nlst[0][0][0]][nlst[0][0]]
-         var.append(nlst[-1])
-         dct[nlst[0][0[0]][nlst[0][0]] = var
-  return dct
+def mouly(gg,k):
 
-PB = ['Bob', '01932342392', 'Alice', '01546734123', 'Britney', '01303544535', 'Aeron', '01723454642', 'Smith', '01923457890', 'Tarek', '01934663922', 'Carol', '01823456785']
-print(phonebook(PB))
+  gg = gg+3 +k
+  return gg
+
+k = mouly(3,4)
+print(k)
